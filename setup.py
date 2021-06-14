2933349537a37a222a08c91b96509a42fc080417
Praneeth Karempudi <praneeth.karempudi@gmail.com>
2021-06-14 17:03:52 +0200

    Project structure setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name='Narsil',
+        version='0.1.0',
+        author='Praneeth Karempudi',
+        author_email='praneeth.karempudi@gmail.com',
+        license='LICENSE.txt',
+        description='The package to work with microscopy data of prokaryotic cells',
+        long_description=open('README.md').read(),
+        )
