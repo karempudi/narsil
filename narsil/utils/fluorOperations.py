212a26d051938b4411c5ce283ba27a6125ecc189
Praneeth Karempudi <praneeth.karempudi@gmail.com>
2021-06-17 17:52:35 +0200

    Fluor operations to generate data for seg nets done

--- narsil/utils/fluorOperations.py
+++ narsil/utils/fluorOperations.py
@@ -8,11 +8,49 @@ from skimage.measure import label, regionprops
 from skimage.morphology import remove_small_objects
 import glob
 import pathlib
+from skimage.restoration import rolling_ball
+from skimage.filters import difference_of_gaussians, gaussian
+from skimage.exposure import equalize_hist
+from skimage.morphology import area_opening
+from skimage.feature import peak_local_max
+from skimage.segmentation import watershed
+from skimage.util.dtype import img_as_ubyte
+from skimage import img_as_ubyte
 
-# Function to generate weight maps from a binary segmentation file
-def generateWeights(filename):
-	pass
+# Function to generate weight maps from a binary segmentation file. 
+# The binary segmenation where dtype is uint8 with true values == 255
+def generateWeights(filename, sigma = 5, w0 = 10):
+	img = imread(filename)
+	labeledImg, num = label(img, return_num=True, connectivity=2)
+	labeledImg = remove_small_objects(labeledImg, min_size=250)
+	unique_values = np.unique(labeledImg)
+	num_values = len(unique_values)
+	h, w = labeledImg.shape
+	stack = np.zeros(shape=(num_values, h, w))
+	for i in range(num_values):
+		stack[i] = ndi.distance_transform_edt(~(labeledImg == unique_values[i]))
+	sorted_distance_stack = np.sort(stack, axis=0)
+	distance_sum = sorted_distance_stack[1] + sorted_distance_stack[2]
+	squared_distance = distance_sum ** 2/ (2 * (sigma**2))
+	weightmap = w0 * np.exp(-squared_distance)*(labeledImg == 0)
+	return weightmap
 
-#
-def generateBinaryMaskFromFluor():
-	pass
+# Used for generating binary fluorescent mask from fluorescence data,
+# Tested against Pseudomonas data only
+def generateBinaryMaskFromFluor(fluorImgfilename):
+	fluorImg = imread(fluorImgfilename).astype('float32')
+	background = rolling_ball(fluorImg, radius = 50)
+	fluorImg_remback = fluorImg - background
+	filtered = difference_of_gaussians(fluorImg_remback, 1, 4)
+	fluorImg_scaled_eq = equalize_hist(filtered)
+	filtered_gaussian = gaussian(fluorImg_scaled_eq)
+	filtered_median = ndi.median_filter(filtered_gaussian, size=5)
+	filtered_gaussian  = filtered_gaussian > 0.92
+	image_opening = area_opening(filtered_gaussian, area_threshold=150)
+	distance = ndi.distance_transform_edt(image_opening)
+	coords = peak_local_max(distance,min_distance = 90, footprint=np.ones((7,7)), labels=image_opening)
+	mask = np.zeros(distance.shape, dtype=bool)
+	mask[tuple(coords.T)] = True
+	markers, _ = ndi.label(mask)
+	labels = watershed(-distance, markers, mask=image_opening, watershed_line=True, connectivity=2)
+	return img_as_ubyte(labels > 0)
